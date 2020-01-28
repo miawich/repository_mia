b0fc24f374bc873c98527bb645c0800460eaea53
miawichman <mwichman@fwparker.org>
2020-01-28 13:28:16 -0600

    added imports notes

---
+++ Notes/01_imports.py
@@ -0,0 +1,27 @@
+#  imports and libraries - 01/28/20
+
+import math
+import random as r
+from math import *
+from import_me import settings
+from import_me.settings import *
+import pandas as pd
+
+if __name__ == "__main__":
+    #  this code only runs if you open THIS FILE
+
+    #  import library directly
+    print(math.pi)
+
+    #  import and assign a shortcut name
+    print(r.randrange(1, 7))
+
+    #  import everything using wildcard: *
+    print(pi)
+
+    #  importing your own python files (modules)
+
+    print(settings.a)
+    print(a)
+
+    print(double(a))
