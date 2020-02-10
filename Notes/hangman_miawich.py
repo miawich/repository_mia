feba7f0d86d2d76e708f59be11ddbe5e97bd8b1d
miawichman <mwichman@fwparker.org>
2020-02-10 09:54:27 -0600

    02/06/20: hangman and other things

--- Notes/hangman_miawich.py
+++ Notes/hangman_miawich.py
@@ -1,14 +1,15 @@
 # Hangman: 02/03/20
 import random as r
 done = False
-my_words = ["boogie woogie", "coffee", "mountain", "soprano", "bill nye"]
+my_words = ["hike", "cafe", "plants", "music", "thermos", "kahlo"]
 my_words = [x.upper() for x in my_words]
 r.shuffle(my_words)
 my_word = my_words.pop()
 misses = []
 right = []
-
-
+abcs = [chr(x) for x in range(65, 65 + 26)]
+correct = False
+winning = len(my_word) + 2
 gallows = [
     '''
       +---+
@@ -74,34 +75,47 @@ gallows = [
     =========
     '''
     ]
+end_of_game = gallows[6]
 
-abcs = [chr(x) for x in range(65, 65 + 26)]
 print("welcome to hangman!")
 
 while not done:
     print(gallows[len(misses)])
     print("wrong letters:", misses)
 
+    for letter in my_word:
+        if letter in right:
+            print(letter, end=" ")
+            correct = True
+            for correct in right:
+                winning -= 1
+        else:
+            print("_", end=" ")
+            correct = False
+
+    print()
+
     choice = input("choose your letter:").upper()
 
     if choice in abcs:
         if choice not in my_word:
             misses.append(choice)
+            print("incorrect")
         elif choice in my_word:
             right.append(choice)
+    else:
+        print("that isn't a letter")
 
-    for letter in my_word:
-        if letter in right:
-            print(letter, end=" ")
-            correct = True
-        else:
-            print("_", end=" ")
-            correct = False
-
-
-
-
-
-
+    if len(misses) >= 6:
+        done = True
+        print("you lose")
+        '''
+        replay = input("would you like to play again?")
+        if choice.upper() == "yes":
+            done = False
+            '''
+    if winning <= 0:
+        done = True
+        print("that is correct, you win!")
 
 
