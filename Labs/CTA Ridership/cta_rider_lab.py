71125a75e804a8f51e584a908ff323dd25241eec
miawichman <mwichman@fwparker.org>
2020-03-23 18:56:45 -0500

    03/23/20

--- Labs/CTA Ridership/cta_rider_lab.py
+++ Labs/CTA Ridership/cta_rider_lab.py
@@ -14,3 +14,40 @@ It has been updated with 2018 data, but not yet with 2019 unfortunately
 5  Add a legend to show data represented by each of the three lines. (4pts)
 6  What trend or trends do you see in the data?  Offer a hypotheses which might explain the trend(s). Just add a comment here to explain. (2pts)
 '''
+import matplotlib.pyplot as plt
+import csv
+
+with open("CTA_-_Ridership_-_Annual_Boarding_Totals (1).csv") as f:
+    cr = csv.reader(f)
+    data = list(cr)
+
+print(len(data))
+headers = data.pop(0)
+print(headers)
+
+data.sort(key=lambda x: int(x[0]))
+
+last_ten = data[-11:]
+years = [int(x[0]) for x in last_ten]
+rail = [int(x[3]) for x in last_ten]
+bus = [int(x[1]) for x in last_ten]
+total = [int(x[4]) for x in last_ten]
+
+plt.figure(1, tight_layout=True)
+plt.plot(years, rail, color='pink', label='Train')
+plt.plot(years, bus, color='blue', label='Bus')
+plt.plot(years, total, color='darkgreen', label='Total')
+
+plt.title('CTA Annual Boarding Totals from 2008 to 2018: Train, Bus, and Total')
+plt.xlabel('Years')
+plt.ylabel('Yearly Total Boardings')
+
+plt.legend()
+plt.show()
+
+# HYPOTHESES:
+# one thing i would think about is that in 2008 the recession had just hit,
+# so it was more affordable to take CTA than own a car
+# maybe the city has become more centralized so more people are walk, but it's more likely there are more cars
+# when is the more used transport going to switch to trains?
+# i guess buses are more popular because they can take you to more exact locations and maybe run more frequently
