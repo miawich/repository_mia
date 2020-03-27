7a370090ff9144cd1756e824ec4b44aadd2a9591
miawichman <mwichman@fwparker.org>
2020-03-27 11:30:30 -0500

    03/27/20

--- Labs/CTA Ridership/cta_rider_lab.py
+++ Labs/CTA Ridership/cta_rider_lab.py
@@ -35,19 +35,22 @@ total = [int(x[4]) for x in last_ten]
 
 plt.figure(1, tight_layout=True)
 plt.plot(years, rail, color='pink', label='Train')
-plt.plot(years, bus, color='blue', label='Bus')
-plt.plot(years, total, color='darkgreen', label='Total')
+plt.plot(years, bus, color='green', label='Bus')
+plt.plot(years, total, color='purple', label='Total')
 
-plt.title('CTA Annual Boarding Totals from 2008 to 2018: Train, Bus, and Total')
-plt.xlabel('Years')
-plt.ylabel('Yearly Total Boardings')
+plt.title('CTA Annual Boarding Totals from 2008 to 2018: Train, Bus, and Total', fontsize=10)
+plt.xlabel('Years', fontsize=10)
+plt.ylabel('Yearly Total Boardings', fontsize=10)
 
 plt.legend()
 plt.show()
 
 # HYPOTHESES:
 # one thing i would think about is that in 2008 the recession had just hit,
-# so it was more affordable to take CTA than own a car
+# so it was more affordable to take CTA than own a car.
 # maybe the city has become more centralized so more people are walk, but it's more likely there are more cars
 # when is the more used transport going to switch to trains?
 # i guess buses are more popular because they can take you to more exact locations and maybe run more frequently
+
+# maybe after all the corona-isolating it will be hard to return to normal life of using public transport but it's
+# necessary and some people's only transport option so i think public transport rates will pick up again
--- Notes/NotesB/11_scatter_plot_miawich.py
+++ Notes/NotesB/11_scatter_plot_miawich.py
@@ -1,3 +1,41 @@
 import csv
-import matplotlib
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open("World firearms murders and ownership - Sheet 1.tsv") as f:
+    reader = csv.reader(f, delimiter='\t')
+    data = list(reader)
+
+
+header = data.pop(0)
+print(header)
+
+# scatter of firearms per 100 against homicides per 100k
+
+homicide_100k = []
+firearms_100 = []
+countries = []
+similar = ["Canada", "England and Wales", "Japan", "South Korea", "France", "Germany", "Ireland", "Singapore", "Hungary",
+           "Taiwan", "Israel", "Belgium", "Finland", "Switzerland", "Netherlands", "United States"]
+
+data = [x for x in data if x[0] in similar]
+
+for country in data:
+    try:
+        homicides = float(country[5])
+        firearms = float(country[-2])
+        name = country[0]
+        homicide_100k.append(homicides)
+        firearms_100.append(firearms)
+        countries.append(name)
+    except:
+        print(country[0], "data is inadequate")
+
+
+plt.figure("Homicides per Firearm Worldwide", figsize=(10, 6))
+plt.scatter(firearms_100, homicide_100k)
+plt.ylabel("homicides per 100k population")
+plt.xlabel("firearms per 100 people")
+for i in range(len(countries)):
+    plt.annotate(countries[i], xy=(firearms_100[i], homicide_100k[i]))
+plt.show()
