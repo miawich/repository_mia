14d6bd7f7e5f48fb901f0761f1eef064aad03835
miawichman <mwichman@fwparker.org>
2020-05-18 11:06:56 -0500

    05/18/20

---
+++ Notes/NotesB/15_panda_plotting.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv('/Users/miawichman/PycharmProjects/P2_SP20/Notes/NotesB/titanic.csv')
+# aggregate functions: min, max, mean, median, count, sum
+# works on series
